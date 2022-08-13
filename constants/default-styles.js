a11dd320c6427ad761c923361167f6f048f239d0
ricardom98 <r.moralesbus@gmail.com>
2022-08-13 11:20:20 -0600

    estilos y colores

---
+++ constants/default-styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scroll: {
+        minHeight: '100%',
+    },
+    screen: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingBottom: 36,
+    },
+    section:{
+        width: '85%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+});
+
+export const Colors = {
+    aliceblue: '#f0f8ff',
+    dodgerblue: '#1e90ff',
+    lightgreen: '#90ee90',
+}
