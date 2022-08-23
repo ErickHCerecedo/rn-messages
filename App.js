31c500f2f436e273615ae2d39711497947a75532
ricardom98 <r.moralesbus@gmail.com>
2022-08-23 16:43:58 -0600

    Base para comenzar etapa 2

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
-import { StyleSheet, View, Image } from 'react-native';
-
-// Importación de caja de texto de correo y contraseña
-import InputCorreo from './components/InputCorreo';
-import InputContraseña from './components/InputContraseña';
-
-// Importación de botón de inicio de sesión
-import ButtonStandard from './components/ButtonStandard';
-
-import ContactItem from './components/ContactItem';
-import MessageControl from './components/MessageControl';
-
-import DefaultStyles from './constants/default-styles';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>rn-messages</Text>
     </View>
   );
 }
-
 
 
 const styles = StyleSheet.create({
@@ -27,12 +15,5 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: '20%',
     paddingHorizontal: 40,
-  },
-  image: {
-    height: 100,
-    width: 100,
-    margin: 100,
-    alignItems: 'center',
-    marginBottom: 50
   },
 });
