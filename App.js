2e47ffca150db97f2fd50ab29b3e2f6af08e2cd3
Erick de Jesus Hernandez Cerecedo <hunter@Hunter.home>
2022-08-23 16:51:05 -0500

    Modificaciones

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ContactItem from './components/ContactItem';
+import MessageControl from './components/MessageControl';
+
+import DefaultStyles from './constants/default-styles';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={DefaultStyles.section}>
+        <MessageControl icon={"send"}></MessageControl>
+      </View>
     </View>
   );
 }
